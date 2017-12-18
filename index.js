3191013739aae0da93a88bb3fba9cdb4838ca2cb
Benjamin Woodruff <github@benjam.info>
2017-12-17 22:13:54 -0800

    Set window title to title of active session

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const DEFAULT_TITLE = 'Hyper';
+const SESSION_SET_XTERM_TITLE = 'SESSION_SET_XTERM_TITLE';
+const SESSION_SET_ACTIVE = 'SESSION_SET_ACTIVE';
+const SESSION_ADD = 'SESSION_ADD';
+
 exports.decorateBrowserOptions = defaults => Object.assign({}, defaults, {
   frame: true,
   autoHideMenuBar: true,
@@ -31,4 +36,28 @@ exports.decorateConfig = config => {
       ${config.css || ''}
     `,
   });
+}
+
+function setTitle(title) {
+  console.log(title);
+  const win = require('electron').remote.getCurrentWindow();
+  win.setTitle(title || DEFAULT_TITLE);
+}
+
+exports.reduceSessions = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case SESSION_SET_XTERM_TITLE:
+      if (action.uid === state.activeUid) {
+        setTitle(action.title.trim());
+      }
+      break;
+    case SESSION_SET_ACTIVE:
+      setTitle(state.sessions[action.uid].title);
+      break;
+    case SESSION_ADD:
+      setTitle('');
+      break;
+  }
+  return state;
 }
